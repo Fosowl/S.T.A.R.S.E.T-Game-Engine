48006a7f659676c5c97e6e251be31a1a82ec5733
Martin <martin.legrand@epitech.eu>
2020-04-03 16:35:41 +0200

    [feat] terrain analyser based on average color

--- engine/engine-src/component/terrain_scan.c
+++ engine/engine-src/component/terrain_scan.c
@@ -8,22 +8,29 @@
 #include "../../include/starset-engine.h"
 #include "../../include/internal.h"
 
-static void average_color_2(sfVector2i vector, int *x, int *y, sfVector2u size)
+typedef struct color_ref_t
+{
+    sfColor color;
+    char *name;
+} color_ref_t;
+
+
+static void avoid_memory_void(sfVector2i vector, sfVector2i *position
+, sfVector2u size)
 {
     if (vector.x > 0)
-        *x = vector.x;
-    if (*x >= size.x)
-        *x = size.x;
+        position->x = vector.x;
+    if (position->x >= size.x)
+        position->x = size.x;
     if (vector.y > 0)
-        *y = vector.y;
-    if (*y > size.y)
-        *y = size.y;
+        position->y = vector.y;
+    if (position->y > size.y)
+        position->y = size.y;
 }
 
-sfColor average_color(sfImage *image, sfVector2i vector, sfVector2u size)
+sfColor internal__image_average_color(sfImage *image, sfVector2i vector, sfVector2u size)
 {
-    unsigned int x = 2;
-    unsigned int y = 2;
+    sfVector2u position;
     sfColor right = {255, 255, 255, 255};
     sfColor left = {255, 255, 255, 255};
     sfColor up = {255, 255, 255, 255};
@@ -32,12 +39,12 @@ sfColor average_color(sfImage *image, sfVector2i vector, sfVector2u size)
     sfColor average = {255, 255, 255, 255};
     sfVector2u image_size = sfImage_getSize(image);
 
-    average_color_2(vector, &x, &y, image_size);
-    center = sfImage_getPixel(image, x, y);
-    right = sfImage_getPixel(image, x + (size.x / 2), y);
-    left = sfImage_getPixel(image, x - (size.x / 2), y);
-    up = sfImage_getPixel(image, x, y - (size.y / 2));
-    down = sfImage_getPixel(image, x, y + (size.y / 2));
+    avoid_memory_void(vector, &position, image_size);
+    center = sfImage_getPixel(image, position.x, position.y);
+    right = sfImage_getPixel(image, position.x + (size.x / 2), position.y);
+    left = sfImage_getPixel(image, position.x - (size.x / 2), position.y);
+    up = sfImage_getPixel(image, position.x, position.y - (size.y / 2));
+    down = sfImage_getPixel(image, position.x, position.y + (size.y / 2));
     average.a = (center.a + right.a + left.a + up.a + down.a) / 5;
     average.b = (center.b + right.b + left.b + up.b + down.b) / 5;
     average.g = (center.g + right.g + left.g + up.g + down.g) / 5;
@@ -45,8 +52,53 @@ sfColor average_color(sfImage *image, sfVector2i vector, sfVector2u size)
     return (average);
 }
 
-char *component__terrain_scanner(component_t *component, ...)
+char *internal__terrain_analyser(sfColor average)
 {
+    color_ref_t reference[4];
+    int difference = 1000;
+    int smallest = 1000;
+    int index = 0;
 
+    reference[0].name = fill("grass");
+    reference[0].color = (sfColor){0, 255, 0, 0};
+    reference[1].name = fill("water");
+    reference[1].color = (sfColor){0, 127, 255, 0};
+    reference[2].name = fill("dirt");
+    reference[2].color = (sfColor){0, 255, 0, 0};
+    reference[3].name = fill("road");
+    reference[3].color = (sfColor){64, 64, 64, 0};
+    reference[4].name = fill("black");
+    reference[4].color = (sfColor){0, 0, 0, 0};
+    for (int i = 0; i < 5; i++) {
+        difference = sqrt(pow(reference[i].color.r - average.r, 2)+pow
+        (reference[i].color.g - average.g, 2)+pow
+        (reference[i].color.b - average.b, 2));
+        if (difference < smallest) {
+            smallest = difference;
+            index = i;
+        }
+    }
+    return reference[index].name;
+}
+
+char *component__terrain_scanner(entities_t *entitie, ...)
+{
+    va_list list;
+    long long int arg[10];
+    sfImage *image;
+    sfColor average;
+    sfVector2i vector;
+    char *terrain = NULL;
+
+    vector.x = (int)entitie->position.x;
+    vector.y = (int)entitie->position.y;
+    va_start(list, entitie);
+    for (int i = 0; i < 1; i++)
+        arg[i] = va_arg(list, long long int);
+    image = (sfImage *)arg[0];
+    average = internal__image_average_color(image, vector, entitie->size);
+    terrain = internal__terrain_analyser(average);
+    va_end(list);
+    return (terrain);
 }
 
