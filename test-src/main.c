e9805453c6d9de416b04fcaa2f15724f0a389481
Martin <martin.legrand@epitech.eu>
2020-04-13 10:40:24 +0200

    [feat] update main for testing engine

--- test-src/main.c
+++ test-src/main.c
@@ -10,22 +10,22 @@
 
 int animator_set(entities_t *entities)
 {
-    starset_add_animation(entities, "zombie", "down", (sfVector2u){48, 48});
+    starset_add_animation(entities, "zombie", "down", (sfVector2u){25, 48});
     starset_add_animation_key(entities, "zombie", "down", (sfVector2f){90, 0});
     starset_add_animation_key(entities, "zombie", "down", (sfVector2f){123, 0});
     starset_add_animation_key(entities, "zombie", "down", (sfVector2f){156, 0});
     starset_add_animation_key(entities, "zombie", "down", (sfVector2f){189, 0});
-    starset_add_animation(entities, "zombie", "left", (sfVector2u){48, 48});
-    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){100, 50});
-    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){133, 50});
-    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){166, 50});
-    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){199, 50});
-    starset_add_animation(entities, "zombie", "right", (sfVector2u){48, 48});
+    starset_add_animation(entities, "zombie", "left", (sfVector2u){25, 48});
+    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){90, 50});
+    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){123, 50});
+    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){156, 50});
+    starset_add_animation_key(entities, "zombie", "left", (sfVector2f){189, 50});
+    starset_add_animation(entities, "zombie", "right", (sfVector2u){25, 48});
     starset_add_animation_key(entities, "zombie", "right", (sfVector2f){100, 108});
     starset_add_animation_key(entities, "zombie", "right", (sfVector2f){133, 108});
     starset_add_animation_key(entities, "zombie", "right", (sfVector2f){166, 108});
     starset_add_animation_key(entities, "zombie", "right", (sfVector2f){199, 108});
-    starset_add_animation(entities, "zombie", "up", (sfVector2u){48, 48});
+    starset_add_animation(entities, "zombie", "up", (sfVector2u){25, 48});
     starset_add_animation_key(entities, "zombie", "up", (sfVector2f){100, 108});
     starset_add_animation_key(entities, "zombie", "up", (sfVector2f){133,108});
     starset_add_animation_key(entities, "zombie", "up", (sfVector2f){166,108});
@@ -41,14 +41,18 @@ int main (int ac, char **argv)
     sfVector2f mouse_pos;
     int b = 0;
 
+    window = sfRenderWindow_create(mode, "ENGINE test", sfResize | sfClose, NULL);
+    sfRenderWindow_setFramerateLimit(window, 60);
+
     entities_t *object_list = starset_entities_add(NULL, "./assets/back.jpg", "background", 1);
     starset_entities_get_propreties(object_list, "background")->is_trigger = 1;
     starset_entities_teleport(object_list, "background", 500.0f, 350.0f);
     object_list = starset_entities_add(object_list, "./assets/zombie.jpg", "zombie:marc", 0);
     object_list = starset_entities_add(object_list, "./assets/zombie.jpg", "zombie:daniel", 0);
+    object_list = starset_entities_add(object_list, "./assets/zombie.jpg", "zombie:jean", 0);
+    object_list = starset_entities_add(object_list, "./assets/zombie.jpg", "zombie:mathis", 0);
+    starset_entities_teleport(object_list, "daniel", 100.0f, 150.0f);
     starset_entities_get_propreties(object_list, "zombie")->speed = 5;
-    window = sfRenderWindow_create(mode, "ENGINE test", sfResize | sfClose, NULL);
-    sfRenderWindow_setFramerateLimit(window, 60);
     animator_set(object_list);
     while (sfRenderWindow_isOpen(window)) {
         while (sfRenderWindow_pollEvent(window, &event))
@@ -58,8 +62,11 @@ int main (int ac, char **argv)
             break;
         mouse_pos.x = (float)sfMouse_getPositionRenderWindow(window).x;
         mouse_pos.y = (float)sfMouse_getPositionRenderWindow(window).y;
-        starset_entities_move(object_list, "zombie", mouse_pos.x, mouse_pos.y);
-        starset_play_animation(object_list, "zombie", "up", 4);
+        starset_entities_move_to_other(object_list, "mathis", "marc");
+        starset_entities_move_to_other(object_list, "jean", "marc");
+        starset_entities_move_to_other(object_list, "daniel", "marc");
+        starset_entities_move(object_list, "marc", mouse_pos.x, mouse_pos.y);
+        starset_play_animation(object_list, "zombie", "left", 5);
         starset_update_engine(object_list, window, NULL);
         sfRenderWindow_display(window);
         sfRenderWindow_clear(window, sfBlack);
